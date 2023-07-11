ba750fc37558a0394aaa360e12203f3a149e32d8
yuuki4682 <132807007+yuuki4682@users.noreply.github.com>
2023-07-11 04:20:01 +0000

    finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :is_matching_login_user, only: [:edit, :update]
   
   def after_sign_in_path_for(resources)
     post_images_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,28 +5,30 @@ class UsersController < ApplicationController
   end
 
   def edit
-    user = User.find(params[:id])
-    unless user.id == current_user
-      redirect_to post_image_path
+    unless user.id == current_user.id
+      redirect_to post_images_path
     end
     
     @user = User.find(params[:id])
   end
   
   def update
-    user = user.find(params[:id])
-    unless user == current_user.id
-      redirect_to post_image_path
-    end
-    
     user = User.find(params[:id])
     user.update(user_params)
     redirect_to user_path(user.id)
   end
   
   private
+  
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
   
 end
